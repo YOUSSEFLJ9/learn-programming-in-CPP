c9c5b4e529e534eba849cc9769a06ddb0a88e7cf
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-31 17:46:40 +0100

    Short_hand_if

---
+++ CPP_lv2/Short_hand_if.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+     int input;
+     string result;
+     cout << "please enter a Number \n";
+     cin >> input;
+    result = input == 0? "its Zero" : input > 0? "its Positive" : "its Negative" ;
+    cout << result<<endl;
+    return (0);
+}
