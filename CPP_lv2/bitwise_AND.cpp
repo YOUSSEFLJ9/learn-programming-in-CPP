76f1beda1331289951492986a03d4d337999888f
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-31 20:19:25 +0100

    bitwise OR

--- CPP_lv2/bitwise_AND.cpp
+++ CPP_lv2/bitwise_AND.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-     cout << "results : " << (2 & 5) <<endl;
+     cout << "results : " << (12 & 25) <<endl;
      return (0);
 }
---
+++ CPP_lv2/bitwise_OR.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     cout << "results : " << (12 | 25) <<endl;
+     return (0);
+}
