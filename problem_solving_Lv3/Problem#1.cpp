f6dd76eb4bf8b051bc52b6e0fffbbeb73a392e42
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-07-15 23:09:43 +0100

    problem solving lvl 3

---
+++ problem_solving_Lv3/Problem#1.cpp
@@ -0,0 +1,37 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int randomNumber(int from, int to)
+{
+int Rand = rand()%(to - from +1) +from;
+return (Rand);
+}
+void fillmatrix(int matrix[3][3])
+{
+    for(int i = 0 ; i < 3; i++)
+        {
+            for (int j = 0;  j < 3; j++)
+            {
+                matrix[i][j] = randomNumber(0, 100);
+            }
+            
+        }
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    int matrix[3][3];
+
+    fillmatrix(matrix);
+    for(int i = 0; i <3 ; i++)
+    {
+        for(int j = 0; j<3 ; j++)
+        {
+            cout<<setw(3)<<matrix[i][j]<<setw(3);
+        }
+        cout<<"\n";
+    }
+}
